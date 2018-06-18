7c0fd3a83d43bdfec3015a0a5bebb429738deec2
Miguel Coba <miguel.coba@gmail.com>
2018-06-18 22:28:44 +0200

    Changes app name

--- src/Main.elm
+++ src/Main.elm
@@ -37,7 +37,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        , h1 [] [ text "Welcome to Toltec" ]
         ]
 
 
